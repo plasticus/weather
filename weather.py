0108414fdd5ca2492aa485edd245e80c6c0598e8
ratking <wrvanweelden@gmail.com>
2023-04-04 12:50:00 -0500

    Updated html to use change_message for future weather.  Updated weather.py to include a wind_speed_mph variable and new_wind_speed_mph to properly display MPH in output

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from PIL import Image
 import random
+import math
 
 app = Flask(__name__)
 
@@ -8,6 +9,7 @@ app = Flask(__name__)
 conditions = ['Sunny', 'Partly sunny', 'Partly cloudy', 'Light showers', 'Showers', 'Thunderstorms']
 temperatures = range(70, 85) # Temperature range in Fahrenheit
 wind_speeds = range(5, 30) # Wind speed range in knots
+
 
 # Define the path where the weather condition images are stored
 IMAGE_PATH = 'static/images/'
@@ -18,6 +20,7 @@ def weather():
     condition = random.choice(conditions)
     temperature = random.choice(temperatures)
     wind_speed = random.choice(wind_speeds)
+    wind_speed_mph = int(math.ceil(wind_speed * 1.15078))
 
     # Open the corresponding image for the weather condition
     image_file = IMAGE_PATH + condition.lower().replace(' ', '_') + '.png'
@@ -31,17 +34,19 @@ def weather():
         new_condition = random.choice(conditions)
         new_temperature = random.choice(temperatures)
         new_wind_speed = random.choice(wind_speeds)
-        change_message = f"The weather is expected to change to {new_condition} with a temperature of {new_temperature}F and wind speed of {new_wind_speed} knots ({round(new_wind_speed * 1.15078)} mph) in the evening."
+        new_wind_speed_mph = int(math.ceil(new_wind_speed * 1.15078))
+        change_message = f"The weather is expected to change to {new_condition} with a temperature of {new_temperature}F and wind speed of {new_wind_speed} knots ({new_wind_speed_mph} mph) in the evening."
     else:
         # Use the current weather conditions for the evening
         new_condition = condition
         new_temperature = temperature
         new_wind_speed = wind_speed
+        new_wind_speed_mph = wind_speed_mph
         change_message = "The weather is expected to remain the same in the evening."
 
     # Render the HTML template with the weather information and image
-    return render_template('weather.html', condition=condition, temperature=temperature, wind_speed=wind_speed,
-                           new_condition=new_condition, new_temperature=new_temperature, new_wind_speed=new_wind_speed,
+    return render_template('weather.html', condition=condition, temperature=temperature, wind_speed=wind_speed, wind_speed_mph=wind_speed_mph,
+                           new_condition=new_condition, new_temperature=new_temperature, new_wind_speed=new_wind_speed, new_wind_speed_mph=new_wind_speed_mph,
                            change_message=change_message, image=image_file)
 
 if __name__ == '__main__':
